8130fa2a9f8db50c33964b79692e490162855fe3
Jeroen Baert <3607063+Forceflow@users.noreply.github.com>
2020-05-22 02:53:41 +0200

    Missing semicolon in shader

--- src/shaders/textured_vertex.glsl
+++ src/shaders/textured_vertex.glsl
@@ -2,7 +2,7 @@
 
 // Layout: get attrib at location X and make it available as Y
 layout (location = 0) in vec3 position;
-layout (location = 1) in vec3 color
+layout (location = 1) in vec3 color;
 layout (location = 2) in vec2 texCoord;
 
 out vec3 ourColor;
